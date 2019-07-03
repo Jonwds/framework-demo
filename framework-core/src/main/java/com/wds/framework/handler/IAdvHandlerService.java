0742699cdce13652d11a19a280b0cbd0c248ea21
zhuangrui <zhuangrui@suitbim.com>
2019-07-03 19:31:02 +0800

    first commit

---
+++ framework-core/src/main/java/com/wds/framework/handler/IAdvHandlerService.java
@@ -0,0 +1,13 @@
+package com.wds.framework.handler;
+
+import java.io.Serializable;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author zhuangrui
+ * Date: 2019/7/3
+ * Time: 17:21
+ */
+public interface IAdvHandlerService<Entity extends Serializable,PK extends Serializable> extends IBaseHandlerService<Entity> {
+}
